811a4d388971f598fdcd170c66c31917aba52ec8
Himanshu Agrawal <iec2022008@iiita.ac.in>
2025-06-04 15:05:00 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -34,8 +34,6 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .then(() => {
   console.log('Connected to MongoDB');
