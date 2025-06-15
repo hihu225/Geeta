99fc495b0515d93a8dc66a65dace5f496cb6d403
Himanshu Agrawal <iec2022008@iiita.ac.in>
2025-06-15 17:21:08 +0530

    done

--- backend/utils/firebaseAdmin.js
+++ backend/utils/firebaseAdmin.js
@@ -1,6 +1,6 @@
 // backend/utils/firebaseAdmin.js
 const admin = require("firebase-admin");
-const serviceAccount = require("../config/new.json");
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
